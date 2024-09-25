b99f97954a56aecde5ab9c43d74e3a739397ec4d
ngbucsit <ngbucsit@gmail.com>
2024-09-25 22:52:39 +0800

    Restrict to 60 fps

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,8 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        tick = clock.tick(60)
+        dt = tick / 1000
 
 
 if __name__ == "__main__":
